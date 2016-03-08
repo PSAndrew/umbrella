bbc96d0464c990b52251042dd7068df47cefc0bc
Andrew P Sena <a5polo8s@gmail.com>
2016-03-08 14:15:24 -0800

    Update scraper.php

--- php/scraper.php
+++ php/scraper.php
@@ -4,7 +4,7 @@
 
 	$city=str_replace(" ", "", $city);
 
-	$contents=file_get_contents('http://www.weather-forecast.com/locations/'.$city.'/forecasts/latest');
+	$contents=@file_get_contents('http://www.weather-forecast.com/locations/'.$city.'/forecasts/latest');
 
 	if(preg_match('/3 Day Weather Forecast Summary:<\/b><span class="read-more-small"><span class="read-more-content"> <span class="phrase">(.*?)</s', $contents, $matches)){
 		echo $matches[1];
